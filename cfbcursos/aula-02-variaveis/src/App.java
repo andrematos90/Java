bde4b8c1b6f3bb26425931aa02b73fb0436a924d
André Matos <andrermatos90@gmail.com>
2023-10-09 10:49:33 -0300

    aula 02 - variáveis, comandos de impressao

--- cfbcursos/aula-02-variaveis/src/App.java
+++ cfbcursos/aula-02-variaveis/src/App.java
@@ -1,5 +1,14 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        int numero =  10;
+        String nome = "Simba";
+        double numeroReal = 1.99;
+        boolean estaChovendo = false;
+
+        System.out.printf("o numero é %d e o nome é %s", numero, nome); // é usado para formatar a saída.
+        System.out.println(numeroReal); //quebra linha no final
+        System.out.print("ola ");// imprime o conteúdo entre parênteses no console 
+        System.out.print("mundo"); // mas não adiciona uma quebra de linha no final.
+        System.out.print(estaChovendo);
     }
 }
