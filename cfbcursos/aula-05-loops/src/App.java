65e273c082c97bd4adaf9a967f46c6c082a00826
André Matos <andrermatos90@gmail.com>
2023-10-09 15:26:53 -0300

    aula 05 - loops

--- cfbcursos/aula-05-loops/src/App.java
+++ cfbcursos/aula-05-loops/src/App.java
@@ -1,7 +1,26 @@
 public class App {
     public static void main(String[] args) {
+     /* 
+      for usado quando se sabe a quantidade de iterações
       for(int cont = 0; cont <= 10; cont++){
         System.out.println(cont);
       }
+      */
+
+      /*
+      While usado quando nao se sabe a quantidade necessária de repetições
+      int cont = 0;
+      while(cont < 10){
+        System.out.println(cont);
+        cont ++;
+      }
+       */
+
+       //do while executa o código ao menos uma vez
+      int cont = 11;
+       do {
+        System.out.println(cont);
+        cont ++;
+       } while (cont <= 10);
     }
 }
