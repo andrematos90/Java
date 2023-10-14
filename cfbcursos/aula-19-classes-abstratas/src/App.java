be3ce2392f7123e44067c79172bf2e2c87f01e6d
André Matos <andrermatos90@gmail.com>
2023-10-14 17:38:24 -0300

    Aula 19 - classes abstratas

---
+++ cfbcursos/aula-19-classes-abstratas/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args){
+       Carro carro = new Carro();
+       carro.qtCombustivel(100);
+    }
+}
---
+++ cfbcursos/aula-19-classes-abstratas/src/Veiculo.java
@@ -0,0 +1,6 @@
+public interface Veiculo {
+    public boolean ligar(boolean ligado);
+    public String anda(String anda);
+    public int qtCombustivel(int qt);
+    public void info();
+}
