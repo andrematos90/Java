b756533222a3b053d737ed201ecebe083e5d0ce9
André Matos <andrermatos90@gmail.com>
2023-10-18 10:16:50 -0300

    Aula 34 - jogo da velha parte 2

--- cfbcursos/aula-34-jogo-da-velha-parte-2/src/JogoDaVelha.java
+++ cfbcursos/aula-34-jogo-da-velha-parte-2/src/JogoDaVelha.java
@@ -4,7 +4,7 @@ public class JogoDaVelha {
     public static void main(String[] args){
 
         Campo[][] velha = new Campo[3][3];
-        char simboloatual ='X';
+        char simboloAtual ='X';
         boolean game = true;
         String vitoria = "";
         Scanner scan = new Scanner(System.in);
@@ -16,6 +16,19 @@ public class JogoDaVelha {
                 System.out.printf("Jogador %s venceu%n!", vitoria);
                 break;
             }
+            try{
+                if(verificarJogada(velha, jogar(scan, simboloAtual), simboloAtual)){
+                    if(simboloAtual == 'X'){
+                        simboloAtual = 'O';
+                    }else{
+                        simboloAtual = 'X';
+                    }
+                }
+
+            }catch(Exception e){
+                System.out.print("Erro!");
+            }
+            System.out.print("Fim do jogo");
         }
 
 
@@ -23,7 +36,7 @@ public class JogoDaVelha {
 
 
     public static void desenhaJogo(Campo [][] v){
-        //limpa tela();
+        limparTela();
 
         System.out.println("    0    1    2");
         System.out.printf("0     %c  |  %c   |   %c   %n", v[0][0].getSimbolo(), v[0][1].getSimbolo(), v[0][2].getSimbolo());
@@ -34,7 +47,32 @@ public class JogoDaVelha {
 
     }
 
-    public static void verificaVitoria(velha){
-        
+    public static void limparTela(){
+        for(int cont=0; cont < 200; cont++){
+            System.out.println("");
+        }
+    }
+
+    public static int[] jogar(Scanner scan, char sa){
+        int p[] = new int[2];
+        System.out.printf("%s  %c%n", "Quem joga: ",sa);
+        System.out.print("Informa a linha: ");
+        p[0]=scan.nextInt();
+        System.out.print("Informa a coluna: ");
+         p[1]=scan.nextInt();
+        return p;
+    }
+
+    public boolean verificarJogada(Campo[][] velha, int p[], char simboloAtual){
+        if(velha[p[0]][p[1]].getSimbolo()==' '){
+            velha[p[0]][p[1]].setSimbolo(simboloAtual);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static String verificaVitoria(Campo[][] velha){
+        return "";
     }
 }
