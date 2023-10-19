7cd090c1518ce0f4053f309546593815296f7e89
André Matos <andrermatos90@gmail.com>
2023-10-19 09:35:29 -0300

    ..

---
+++ cfbcursos/aula-36-ArrayList/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+       ArrayList<Integer> numeros = new ArrayList();
+     // Classe/ t.doarray /nomedoarray  /instanciação
+
+    ArrayList<String> nome = new ArrayList();
+    //Classe/t.doarray /nomedoarray  /instanciação
+
+    
+
+
+    }
+    
+}
