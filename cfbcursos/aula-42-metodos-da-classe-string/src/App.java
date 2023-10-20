17f67bd0c3bff48c8224477069bb7b104797f7e4
André Matos <andrermatos90@gmail.com>
2023-10-20 17:11:45 -0300

    Aula 42 - metodos da classe string

--- cfbcursos/aula-42-metodos-da-classe-string/src/App.java
+++ cfbcursos/aula-42-metodos-da-classe-string/src/App.java
@@ -1,5 +1,30 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+       String nome = " Simba ";
+       String raca = "Collie";
+       String texto = "ipsum,lorem,lodsdso";
+
+       /*método replace  permite substituir todas as ocorrências de uma sequência de caracteres (substring) por outra sequência dentro de uma string*/
+       System.out.println(raca.replace('l', 'd'));
+
+       //toLowerCase() converte tudo para minuscula
+       System.out.println(raca.toLowerCase());
+
+       //toUpperCase() converte tudo para maiuscula
+       System.out.println(raca.toUpperCase());
+
+       //trim() remove os espaços do inicio e do fim
+       System.out.println(nome.trim());
+
+       //toCharArray(), converte a string em um array de caracteres
+       char[] c = nome.toCharArray();
+       System.out.println(c[3]);
+
+       //split(),divide uma string em substrings com base em um delimitador específico
+
+       String[] s = texto.split(",");
+       for(String item:s){
+        System.out.println(item);
+       }
     }
 }
