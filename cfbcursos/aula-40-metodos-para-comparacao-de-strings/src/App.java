faa9b7707504a587a561a037e02e8678fe9ffaed
André Matos <andrermatos90@gmail.com>
2023-10-20 10:09:36 -0300

    ..

---
+++ cfbcursos/aula-40-metodos-para-comparacao-de-strings/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        String s1 = new String("Simba");
+        String s2 = "Simba";
+
+        if(s1 == s2){
+            System.out.print("São iguais!");
+        }else{
+            System.out . print("Não sao iguais");
+        }
+    }
+}
