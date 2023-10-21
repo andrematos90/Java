26d6a1d367a7b6992f1ed402fe5775b8515d7cd4
André Matos <andrermatos90@gmail.com>
2023-10-21 09:28:18 -0300

    Aula 44 - lendo arquivos

--- cfbcursos/aula-44-lendo-arquivos/src/App.java
+++ cfbcursos/aula-44-lendo-arquivos/src/App.java
@@ -2,6 +2,7 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 
 public class App {
@@ -9,9 +10,26 @@ public class App {
       
        Path arquivo = Paths.get("C:/Users/andre/Downloads/passos.txt/");
 
-
        if(Files.exists(arquivo)){
-        System.out.println("é um arquivo");
+        try{
+            List<String> linhas = Files.readAllLines(arquivo);
+
+            /*usando for tradicional para imprimir as linhas
+            
+             
+            for(String linha:linhas){
+                System.out.println(linha);
+            }*/
+
+            //usando foreach para ler 
+            linhas.forEach(linha -> System.out.println(linha));
+                      
+        }catch(Exception e){
+            System.out.println("Erro ao ler o arquivo!");
+        }finally{
+            System.out.println("Fim da leitura!");
+        }
+       
        }else{
         System.out.println("nao é um arquivo");
        }
