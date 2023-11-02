f77d075c1679ac4d42148d6477280436b25ae23a
André Matos <andrermatos90@gmail.com>
2023-11-02 16:22:42 -0300

    Criando o Módulo de Cursos e Usando Roteamento com Lazy Loading

--- cursos loaine groner - crud spring  boot + angular/04 - Criando o Módulo de Cursos e Usando Roteamento com Lazy Loading/crud-angular/src/app/courses/courses-routing.module.ts
+++ cursos loaine groner - crud spring  boot + angular/04 - Criando o Módulo de Cursos e Usando Roteamento com Lazy Loading/crud-angular/src/app/courses/courses-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
 
 const routes: Routes = [
-  {path:'', component: CoursesComponent}
+  { path:'', component: CoursesComponent}
 ];
 
 @NgModule({
