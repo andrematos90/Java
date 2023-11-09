1542f53373ed1442f110b4a09cdabec85ef9111d
André Matos <andrermatos90@gmail.com>
2023-11-09 14:31:19 -0300

    ..

--- udemy/APIRestFUll e Microserviços/03 - Construindo uma APIRest-Crud Completo/CRUD - Parte 3/projeto/src/main/java/com/teste/projeto/controller/ProdutoController.java
+++ udemy/APIRestFUll e Microserviços/03 - Construindo uma APIRest-Crud Completo/CRUD - Parte 3/projeto/src/main/java/com/teste/projeto/controller/ProdutoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -14,18 +15,35 @@ public class ProdutoController {
 
     private ProdutoService produtoService;
 
-    public ProdutoController(ProdutoService produtoService){
+    public ProdutoController(ProdutoService produtoService) {
         this.produtoService = produtoService;
     }
 
     @GetMapping
-    public List<ProdutoModel> obterTodosOsProdutos(){
+    public List<ProdutoModel> obterTodosOsProdutos() {
         return produtoService.obterTodosOsProdutos();
 
     }
+
     @PostMapping
-    public ProdutoModel adicionarUmProduto(@RequestBody ProdutoModel produto){
+    public ProdutoModel adicionarUmProduto(@RequestBody ProdutoModel produto) {
         return produtoService.adicionarUmProduto(produto);
     }
 
+    @GetMapping("/{id}")
+    public Optional<ProdutoModel> obterProdutoPorId(@PathVariable Integer id) {
+        return produtoService.obterProdutoPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deletarProduto(@PathVariable Integer id) {
+        produtoService.deletarProduto(id);
+        return String.format("Produto com o id %d deletado com sucesso!", id);
+    }
+
+    @PutMapping("/{id}")
+    public ProdutoModel atualizarProduto(@RequestBody ProdutoModel produto, @PathVariable Integer id) {
+        return produtoService.atualizarProduto(id, produto);
+    }
 }
+
