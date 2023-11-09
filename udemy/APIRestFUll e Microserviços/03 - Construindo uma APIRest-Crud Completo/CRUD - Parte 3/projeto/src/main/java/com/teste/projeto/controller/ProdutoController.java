6e55a3f2236c44e3a457dc984ecdd160c05ff0bd
André Matos <andrermatos90@gmail.com>
2023-11-08 21:06:21 -0300

    ..

--- udemy/APIRestFUll e Microserviços/03 - Construindo uma APIRest-Crud Completo/CRUD - Parte 3/projeto/src/main/java/com/teste/projeto/controller/ProdutoController.java
+++ udemy/APIRestFUll e Microserviços/03 - Construindo uma APIRest-Crud Completo/CRUD - Parte 3/projeto/src/main/java/com/teste/projeto/controller/ProdutoController.java
@@ -4,9 +4,7 @@ package com.teste.projeto.controller;
 import com.teste.projeto.model.ProdutoModel;
 import com.teste.projeto.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +22,10 @@ public class ProdutoController {
     public List<ProdutoModel> obterTodosOsProdutos(){
         return produtoService.obterTodosOsProdutos();
 
+    }
+    @PostMapping
+    public ProdutoModel adicionarUmProduto(@RequestBody ProdutoModel produto){
+        return produtoService.adicionarUmProduto(produto);
     }
 
 }
