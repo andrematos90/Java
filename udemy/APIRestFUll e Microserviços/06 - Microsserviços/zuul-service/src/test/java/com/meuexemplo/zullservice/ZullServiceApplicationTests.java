f1b10a3b900d990b5adbae9e6c30e2e1ce27326c
André Matos <andrermatos90@gmail.com>
2023-11-21 21:04:37 -0300

    service e gateway funcionando

---
+++ udemy/APIRestFUll e Microserviços/06 - Microsserviços/zuul-service/src/test/java/com/meuexemplo/zullservice/ZullServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuexemplo.zullservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZullServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
