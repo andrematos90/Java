528ba669949091be417a3e2fdaebdd458b826cc3
André Matos <andrermatos90@gmail.com>
2024-01-11 16:09:04 -0300

    inicio do projeto

--- cursos devsuperior/curso spring/dslist/src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
+++
@@ -1,8 +0,0 @@
-package com.devsuperior.dslist.repositories;
-
-import com.devsuperior.dslist.entities.GameList;
-import com.devsuperior.dslist.services.GameListService;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameListRepository extends JpaRepository<GameList, Long> {
-}
