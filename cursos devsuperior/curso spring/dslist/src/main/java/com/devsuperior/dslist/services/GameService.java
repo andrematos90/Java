574e40b1bdb9fdf849824ac6f8b8029fc611e6db
André Matos <andrermatos90@gmail.com>
2024-01-23 15:38:34 -0300

    GameDTO e metodo no service e controller

--- cursos devsuperior/curso spring/dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
+++ cursos devsuperior/curso spring/dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dslist.services;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,4 +20,21 @@ public class GameService {
         return dto;
 
     }
+
+    @Transactional(readOnly = true) // é utilizada para indicar que um método deve ser envolvido por uma transação
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+        return dto;
+
+    }
+
+    /* o método retornará um GameDTO (que possui todas as propriedades do game),
+    findById(id); é o método do repository no qual é passado um id e ele retorna
+    um "optional" para extrarir o Game desse optional usa se o ".get() tudo é
+    salvo na variável result que é do tipo Game"
+
+     GameDTO dto = new GameDTO(result);
+     converte de Game para o tipo GameDto e salvo na variável dto que é retornada
+     */
 }
