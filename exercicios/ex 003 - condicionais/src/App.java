c5d68692305fbb01b6ead09c4f3da7daeca45d34
André Matos <andrermatos90@gmail.com>
2024-01-31 11:42:58 -0300

    ex 03

---
+++ exercicios/ex 003 - condicionais/src/App.java
@@ -0,0 +1,34 @@
+/*Você foi contratado para desenvolver um programa que simule um sistema de reserva de passagens de avião. O programa deve permitir que os usuários escolham o tipo de assento e a classe do voo. Existem três tipos de assentos disponíveis: Econômico, Executivo e Primeira Classe. As classes de voo são: Internacional e Nacional.
+
+As regras para a reserva são as seguintes:
+
+Para voos internacionais:
+
+A classe Econômica possui 30 assentos, 5 estão disponiveis.
+A classe Executiva possui 20 assentos, 1 estão disponiveis.
+A Primeira Classe possui 10 assentos, 8 estão disponiveis.
+
+Para voos nacionais:
+
+A classe Econômica possui 50 assentos, 0 estão disponiveis.
+A classe Executiva possui 30 assentos, 5 estão disponiveis.
+A Primeira Classe possui 15 assentos, 5 estão disponiveis.
+
+Seu programa deve:
+
+Solicitar ao usuário que selecione o tipo de voo (Internacional ou Nacional).
+Solicitar ao usuário que selecione a classe do voo (Econômica, Executiva ou Primeira Classe).
+
+Verificar a disponibilidade de assentos para a classe selecionada e informar se a reserva foi bem-sucedida ou se não há assentos disponíveis naquela classe.
+
+Considere também as seguintes restrições:
+
+Caso a classe selecionada não seja válida, informe ao usuário e peça uma nova seleção.
+Caso o tipo de voo selecionado não seja válido, informe ao usuário e peça uma nova seleção. */
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
